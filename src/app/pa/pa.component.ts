8b8ed7979a1ba906797e1648cdd40d9c11f85d61
Ryan Moore <ryamoore@pdx.edu>
2018-08-19 18:18:58 -0700

    Added landing button for the stretch goal pages and routed it to the landing page, also removed home button

--- src/app/pa/pa.component.ts
+++ src/app/pa/pa.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppState } from '../app.service';
 
 @Component({
   selector: 'app-pa',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pa.component.css']
 })
 export class PaComponent implements OnInit {
-
-  constructor() { }
+    public localState = { value: '' };
+    constructor(public appState: AppState, private router: Router) { }
 
   ngOnInit() {
   }
 
+  public patientOnClick() {
+      this.router.navigateByUrl('/landing');
+      console.log('go to landing page');
+  }
 }
