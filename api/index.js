5f1067d0c62aff224aa3a978fd4131a18821cff8
Griffin Stone <gstone@pdx.edu>
2019-04-03 18:40:32 -0700

    Add API handler factory
    * Ensures that authentication and error handling are handled in a
    standardized way
    * Authentication and error handling are yet to be implemented

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
-module.exports = (request, response, next) => {
-    response.json({ info: 'Hello, world!'})
-}
+const handler = require('./handler.js');
+
+module.exports = handler((request) => {
+  return 'Hello, world!';
+});
