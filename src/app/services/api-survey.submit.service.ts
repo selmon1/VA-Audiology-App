b061ab24d1ba9086f9fdb67839b53d16bc0e9aa8
Liam Wynn <wynnliam@pdx.edu>
2019-04-17 11:57:42 -0700

    Added api-survey.submit.service

---
+++ src/app/services/api-survey.submit.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class SurveySubmitHandler {
+  constructor() {
+    console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH');
+  }
+
+  public print() : string {
+    return 'Test';
+  }
+}
