cda30339125b943c640777addf105a96c42ce28c
Liam Wynn <wynnliam@pdx.edu>
2019-04-18 18:03:33 -0700

    Added classes that model the SurveyInstance according to the ER diagram.

--- src/app/services/api-survey.submit.service.ts
+++ src/app/services/api-survey.submit.service.ts
@@ -1,6 +1,55 @@
 import { Injectable } from '@angular/core';
 import { Utilities } from '../common/utlilities';
 
+// TODO: Better name for this!
+class PatientJSON {
+  patienID : number = 0;
+  isDeceased : boolean = false;
+}
+
+class PatientSurveyJSON {
+    otoscopy : string = '';
+    typanometry : string = '';
+
+
+    rightEarLowFreqSeverity : string = '';
+    rightEarLowFreqConfiguration : string = '';
+    rightEarHighFreqSeverity : string = '';
+    rightEarHighFreqConfiguration : string = '';
+
+    leftEarLowFreqSeverity : string = '';
+    leftEarLowFreqConfiguration : string = '';
+    leftEarHighFreqSeverity : string = '';
+    leftEarHighFreqConfiguration : string = '';
+
+    audiogram : string = '';
+
+
+    thsSectionATotal : number = 0;
+    thsSectionBTotal : number = 0;
+    thsSectionCSeverity : string = '';
+
+
+    tfiI : number = 0;
+    tfiSc : number = 0;
+    tfiC : number = 0;
+    tfiSi : number = 0;
+    tfiA : number = 0;
+    tfiR : number = 0;
+    tfiQ : number = 0;
+    tfiE : number = 0;
+
+    tfiOverallScore : number = 0;
+
+
+    tsTinnitusType : string = '';
+}
+
+class SurveyInstanceJSON {
+  patientSurvey : PatientSurveyJSON;
+  patient : PatientJSON;
+}
+
 @Injectable()
 export class SurveySubmitHandler {
   public submitSurvey() : boolean {
@@ -13,6 +62,8 @@ export class SurveySubmitHandler {
     }
 
     console.log(testData);
+    let result = new SurveyInstanceJSON;
+    console.log(JSON.stringify(result));
     return false;
   }
 }
