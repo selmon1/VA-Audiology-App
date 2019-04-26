7c726b17851216f237593c7f26fbef2eb760ee71
Griffin Stone <gstone@pdx.edu>
2019-04-25 17:03:37 -0700

    Move DB creds to another file

--- api/db.ts
+++ api/db.ts
@@ -1,13 +1,8 @@
 import { Pool } from 'pg';
-// TODO: get credentials from a file not included in git
+// creds.ts is not included in git and you must write it yourself; see creds.example
+import dbLogin from './creds';
 
-let connection = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'va_trial',
-  password: 'postgres',
-  port: 5432,
-});
+let connection = new Pool(dbLogin);
 
 export default function makeConnection()
 {
