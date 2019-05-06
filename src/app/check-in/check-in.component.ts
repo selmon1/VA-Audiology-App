ae5bd050f5f527fcffbc67a398624ed233b79e02
Liam Wynn <wynnliam@pdx.edu>
2019-05-06 10:36:07 -0700

    Fixed terrible formatting on check-in.component.ts

--- src/app/check-in/check-in.component.ts
+++ src/app/check-in/check-in.component.ts
@@ -13,48 +13,41 @@ import { ThsDataService } from '../services/ths-data.service';
 })
 
 export class CheckInComponent {
-   public patientId: string = '';
-   public authenticationFlag: boolean = true;
-
-   constructor(private router: Router, private tsDataService: TsScreenerDataService, private tfiDataService: TfiDataService, private thsDataService: ThsDataService) {};
-
-   /**
-    * This function will be call when the "check in" button is pressed.
-    * It evaluates the value is typed in the input area; if the value has the length of 6,
-    * then the page will be route to the "audiologist" page;
-    * if the value of length 4, then the page will be route to the "appointment" page.
-    * If the value has different length, then it will be informed as incorrect check in.
-    */
-
-   public onClick() {
-       if(this.patientId.length === 4) {  
-       sessionStorage.clear();
-       this.tsDataService.clearHistory();
-       this.tfiDataService.clearHistory();
-       this.thsDataService.clearHistory();
-       Utilities.setSessionStorage('patient-id', this.patientId);
-       Utilities.setSessionStorage('firstName', 'DEPRECATED: GET RID OF FIRST NAME!');
-       console.log(this.patientId);
-       this.router.navigateByUrl('/appointments');
-       console.log('log in with ' + this.patientId);
-       } else {
-           this.authenticationFlag = false;
-           this.patientId = '';
-           console.log('failed log in ' + this.patientId);
-       }
-   }
-
-   /**
-    * This function is called when the "enter" key on the keyboard is pressed.
-    * If the key has the value of 13, then this function will call the "onClick" function from above.
-    * @param event the event caught by the action.
-    */
-   public keyDownFunction(event) {
+  public patientId: string = '';
+  public authenticationFlag: boolean = true;
+  
+  constructor(private router: Router, private tsDataService: TsScreenerDataService, private tfiDataService: TfiDataService, private thsDataService: ThsDataService) {};
+  
+  public onClick() {
+    if(this.patientId.length === 4) {  
+      sessionStorage.clear();
+      this.tsDataService.clearHistory();
+      this.tfiDataService.clearHistory();
+      this.thsDataService.clearHistory();
+      Utilities.setSessionStorage('patient-id', this.patientId);
+      Utilities.setSessionStorage('firstName', 'DEPRECATED: GET RID OF FIRST NAME!');
+      console.log(this.patientId);
+      this.router.navigateByUrl('/appointments');
+      console.log('log in with ' + this.patientId);
+    } else {
+      this.authenticationFlag = false;
+      this.patientId = '';
+      console.log('failed log in ' + this.patientId);
+    }
+  }
+  
+  
+  
+  /**
+   * This function is called when the "enter" key on the keyboard is pressed.
+   * If the key has the value of 13, then this function will call the "onClick" function from above.
+   * @param event the event caught by the action.
+  */
+  public keyDownFunction(event) {
     if ( event.keyCode === 13) {
       this.onClick();
     } else {
       this.authenticationFlag = true;
     }
   }
-
-  }
+}
