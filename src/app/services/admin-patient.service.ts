6239e6cbf9d825f29538e4848874a60dd2fcdf68
Liam Wynn <wynnliam@pdx.edu>
2019-05-10 12:23:19 -0700

    Added dummy_heartbeat function to test connection

--- src/app/services/admin-patient.service.ts
+++ src/app/services/admin-patient.service.ts
@@ -3,6 +3,11 @@ import { ServerApiService } from './server-api.service';
 
 @Injectable()
 export class AdminPatientService {
-    constructor(private serverApiService: ServerApiService) { }
+  constructor(private serverApiService: ServerApiService) { }
+
+  public dummy_heartbeat() : void {
+    this.serverApiService.get('/')
+      .subscribe(response => console.log(response));
+  }
 }
 
