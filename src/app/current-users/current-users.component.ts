05040820c37ba34a79caedceba8dc74d8ebc1d11
Mishal <mialajmi@pdx.edu>
2019-05-09 18:51:17 -0700

    Created current-users component

---
+++ src/app/current-users/current-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'current-users',
+  templateUrl: './current-users.component.html',
+  styleUrls: ['./current-users.component.css']
+})
+export class CurrentUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
