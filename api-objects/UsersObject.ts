0f252411a7a252632d5ecd3e18e1449b0f50ac92
Mishal <mialajmi@pdx.edu>
2019-05-10 16:57:29 -0700

    Added visual feedback for user, and refactored checkUserName() to accept object from service

--- api-objects/UsersObject.ts
+++ api-objects/UsersObject.ts
@@ -1,9 +1,10 @@
 export const authorityTypes: Array<string> = ['Admin', 'Audiologist'];
 
 export class UsersObject {
-   public username: string;
-   public name: string;
-   public email: string;
-   public password: string;
-   public authorityType: number;
+   constructor(
+   public username: string,
+   public name: string,
+   public email: string,
+   public password: string,
+   public authorityType: number) { }
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersObject, authorityTypes } from '../../../api-objects/UsersObject';
-// import * as createAccount from '../services/api-users-crud.services.ts'; // Uncomment when available
+// import APIUsersCrudService from '../services/api-users-crud.services'; 
 
 @Component({
   selector: 'users',
@@ -15,63 +15,82 @@ export class UsersComponent implements OnInit {
   public authorityType: number;
   public userEmail: string = '';
   public name: string = '';
-  public usernameTaken: boolean = true;
+  public usernameTaken: boolean = false;
   public showUserInfo: boolean = false;
-  public validEmail: boolean;
+  public validEmail: boolean = true;
 
-  constructor() {}
-  
+
+  // UNCOMMENT when Admin CRUD service is available
+  constructor(/*
+    private UserServices: APICrudService*/) { }
+
   public ngOnInit() {
   }
-  
+
   get authorityTypes() { return authorityTypes; }
 
-/** Checks whether a username exists or not, sets the temp password if the user doesn't exist,
- *  and returns true or false based on if the user exists or not.
- *  @Param username: Checked for uniqueness by the createAccount call
- */
-  public checkUserName(UserRequest: UsersObject): boolean {
+  /**
+   * Checks whether a username exists or not, sets the temp password if the user doesn't exist, sets the usernameTaken flag to true 
+   * if no errors returned, and false if errors are returned.
+   *  @Param UserRequest: Object of type UsersObject which has fields {username,name,email,password,authorityType}
+   */
+  /*
+    private checkUserName(UserRequest: UsersObject): void {
+      this.UserServices.createUsers(UserRequest).subscribe((result) =>{
+        try{
+          this.userPassword = result.password;
+          this.usernameTaken = false;
+        } catch(result.error.exisitingUsername) {
+          this.usernameTaken = true;
+          console.log("Username not Avaliable");
+        }
+      });
+    }
+    */
 
-    // TODO: 
-    return false;
-  }
-
+  /**
+   * Does a regular expression to check whether the email has correct formatting
+   * @param email string that contains the user input representing an email
+   */
   public isInputValid(email: string): boolean {
     let checkEmail = /\S+@\S+\.\S+/;
     return checkEmail.test(String(email));
   }
 
   /**
-   * Sets the authority type based on the selection from the drop down menu
+   * Sets the authorityType based on the user selection from the drop down menu.
+   * @param event event of selecting an option from the drop down menu
    */
-  public chooseAuth(event):void {
+  public chooseAuth(event): void {
     this.authorityType = authorityTypes.indexOf(event.target.value);
   }
 
+  /**
+   * Checks that all fields are valid, then creates a UsersObject and passes it to the createUser service call.
+   */
   public createUser(): void {
 
-    if(this.isInputValid(this.userEmail)) {
+    if(this.isInputValid(this.userEmail) && this.name !== '' && this.username !== '' && (this.authorityType === 0 || this.authorityType === 1)) {
+
       this.validEmail = true;
-    } else {
-      this.validEmail = false;
-      this.showUserInfo = false;
-    }
-
-    if (this.name !== '' && this.username !== '' && this.validEmail !== false) {
       this.showUserInfo = true;
+      this.usernameTaken = false; // TODO: Remove once service is connected
 
-      let UsersCreateRequest: UsersObject = {
-        username: this.username,
-        name: this.name,
-        email: this.userEmail,
-        password: this.userPassword,
-        authorityType: this.authorityType
-      };
+      // Create new user object
+      let UserRequest = new UsersObject(
+        this.username,
+        this.name,
+        this.userEmail,
+        this.userPassword,
+        this.authorityType);
 
-      this.usernameTaken = this.checkUserName(UsersCreateRequest);
+      // this.checkUserName(UserRequest); // TODO: Uncomment once service is connected
 
-      } else {
-        console.log("NO");
-      }
+    } else {
+      this.validEmail = true;
+      this.showUserInfo = false;
+      this.usernameTaken = true; // TODO: Remove once service is connected
+      console.log('NO GOOD');
     }
   }
+}
