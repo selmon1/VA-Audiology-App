eef78b8477eeb7a5224c39f6e46951ce7456bc58
Liam Wynn <wynnliam@pdx.edu>
2019-05-13 23:43:22 -0700

    Replaced user with accounts in urls for admin-patient services

--- src/app/services/admin-patient.service.ts
+++ src/app/services/admin-patient.service.ts
@@ -8,15 +8,15 @@ export class AdminPatientService {
   constructor(private serverApiService: ServerApiService) { }
 
   public createUser(name: string, password: string, type: string) {
-    return this.serverApiService.post<GenericAdminPatientResponse>('/user/create', {'name': name, 'password': password, 'type': type});
+    return this.serverApiService.post<GenericAdminPatientResponse>('/accounts/create', {'name': name, 'password': password, 'type': type});
   }
 
   public renameUser(currName: string, nextName: string) {
-    return this.serverApiService.post<GenericAdminPatientResponse>('/user/rename', {'oldName': currName, 'newName': nextName});
+    return this.serverApiService.post<GenericAdminPatientResponse>('/accounts/rename', {'oldName': currName, 'newName': nextName});
   }
 
   public resetPassword(name: string) {
-    return this.serverApiService.post<ResetPasswordResponse>('/user/resetPassword', { 'name': name });
+    return this.serverApiService.post<ResetPasswordResponse>('/accounts/resetPassword', { 'name': name });
   }
 
   public deleteUser(name: string) {
@@ -24,11 +24,11 @@ export class AdminPatientService {
 
     params.set('name', name);
 
-    return this.serverApiService.delete<GenericAdminPatientResponse>('/user/delete', params);
+    return this.serverApiService.delete<GenericAdminPatientResponse>('/accounts/delete', params);
   }
 
   public listUsers() {
-    return this.serverApiService.get<UserListItem[]>('/user/list');
+    return this.serverApiService.get<UserListItem[]>('/accounts/list');
   }
 }
 
