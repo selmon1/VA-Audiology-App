e10114d4a0be5c7f22174ad3856f05dbef85aef2
Liam Wynn <wynnliam@pdx.edu>
2019-05-14 00:19:02 -0700

    Updated services to specify a proper return type Observable<Response<...>>

--- src/app/services/admin-patient.service.ts
+++ src/app/services/admin-patient.service.ts
@@ -1,33 +1,34 @@
 import { Injectable } from '@angular/core';
 import { ServerApiService } from './server-api.service';
 import { Observable } from 'rxjs';
-import { GenericAdminPatientResponse, ResetPasswordResponse, UserListItem } from '../../../api-objects/AdminPatient';
+import { Response } from '../../../api-objects/GenericResponse';
+import { AdminPatientResponse, ResetPasswordResponse, UserListItem } from '../../../api-objects/AdminPatient';
 
 @Injectable()
 export class AdminPatientService {
   constructor(private serverApiService: ServerApiService) { }
 
-  public createUser(name: string, password: string, type: string) {
-    return this.serverApiService.post<GenericAdminPatientResponse>('/accounts/create', {'name': name, 'password': password, 'type': type});
+  public createUser(name: string, password: string, type: string) : Observable<Response<AdminPatientResponse>> {
+    return this.serverApiService.post<AdminPatientResponse>('/accounts/create', {'name': name, 'password': password, 'type': type});
   }
 
-  public renameUser(currName: string, nextName: string) {
-    return this.serverApiService.post<GenericAdminPatientResponse>('/accounts/rename', {'oldName': currName, 'newName': nextName});
+  public renameUser(currName: string, nextName: string) : Observable<Response<AdminPatientResponse>> {
+    return this.serverApiService.post<AdminPatientResponse>('/accounts/rename', {'oldName': currName, 'newName': nextName});
   }
 
-  public resetPassword(name: string) {
+  public resetPassword(name: string) : Observable<Response<ResetPasswordResponse>> {
     return this.serverApiService.post<ResetPasswordResponse>('/accounts/resetPassword', { 'name': name });
   }
 
-  public deleteUser(name: string) {
+  public deleteUser(name: string) : Observable<Response<AdminPatientResponse>> {
     let params : Map<string, string> = new Map();
 
     params.set('name', name);
 
-    return this.serverApiService.delete<GenericAdminPatientResponse>('/accounts/delete', params);
+    return this.serverApiService.delete<AdminPatientResponse>('/accounts/delete', params);
   }
 
-  public listUsers() {
+  public listUsers() : Observable<Response<UserListItem[]>> {
     return this.serverApiService.get<UserListItem[]>('/accounts/list');
   }
 }
