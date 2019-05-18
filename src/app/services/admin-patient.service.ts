d139ab0ecbdbd58e4542f769c7ecf10c25a2602f
Liam Wynn <wynnliam@pdx.edu>
2019-05-18 16:00:00 -0700

    Updated AdminPatientService to have getPatient(s) return an array of Appointment objects

--- src/app/services/admin-patient.service.ts
+++ src/app/services/admin-patient.service.ts
@@ -4,21 +4,22 @@ import { Observable } from 'rxjs';
 import { Response } from '../../../api-objects/GenericResponse';
 import { AdminPatientResponse, ResetPasswordResponse, UserListItem } from '../../../api-objects/AdminPatient';
 import { PatientResponse } from '../../../api-objects/patientResponse';
+import { Appointment } from '../../../api-objects/Appointment';
 
 @Injectable()
 export class AdminPatientService {
   constructor(private serverApiService: ServerApiService) { }
 
   public createPatient(patientId : string, deceased : boolean, patientNotes : string) : Observable<Response<PatientResponse>> {
-    return this.serverApiService.post('patient', {'patientId': patientId, 'deceased': deceased, 'patientNotes': patientNotes});
+    return this.serverApiService.post('patient', {patientId, deceased, patientNotes});
   }
 
-  public getPatients() : Observable<Response<PatientResponse[]>> {
-    return this.serverApiService.get<PatientResponse[]>('patient');
+  public getPatients() : Observable<Response<Appointment[]>> {
+    return this.serverApiService.get<Appointment[]>('patient');
   }
 
-  public getPatient(patientId : number) : Observable<Response<PatientResponse[]>> {
-    return this.serverApiService.get<PatientResponse[]>('patient/' + patientId);
+  public getPatient(patientId : number) : Observable<Response<Appointment[]>> {
+    return this.serverApiService.get<Appointment[]>('patient/' + patientId);
   }
 
   public deletePatient(patientId : number) : Observable<Response<any>> {
